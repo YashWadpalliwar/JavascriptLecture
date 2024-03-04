4ad07d89a8eb38fc2ee283f4cc7ddf145bfffb6d
Yash Wadpalliwar <yashhardly16@gmail.com>
2024-03-04 14:11:59 +0530

    Asycrocode

--- AdvenceJavascript/Syncro&Asyncro.js
+++ AdvenceJavascript/Syncro&Asyncro.js
@@ -5,25 +5,86 @@ Single threaded.
 Execute one line at a time.
 Ex:-Console.log();1
     Console.log();2
+
 BlokingCode (Synchronous)
+
 & Non BlockingCode (Asynchronous)
 */
 
 /*
 There are 4 main types of Async code
-1)Callback
-2)Asynchronous
-3)Promises 
+1)Asynchronous
+2)Callback
+3)Promises
 4)Async/Await
 */
 
 //Syncronus code.
+/*
 let age = 21;
 let id = 22;
 let rollno = 123;
 console.log(age);
 console.log(id);
 console.log(rollno);
+*/
 
 
+//Asynchronous Code.
+/*
+1)SetTimeOut();
+2)ClearTimeOut();
 
+3)SetInterver();
+4)ClearIntrever();
+*/
+
+//1)SetTimeOut()
+//Ex:1
+/*
+console.log("Aap rutho mai aata hu");
+setTimeout(function (){
+
+    console.log("Haa mai aa gaya");//Asynchronous Code.
+    
+},2000)//2 millisecond.
+console.log("Aap tap tak ke code karo");
+*/
+
+//Ex:-2
+/*
+console.log("Start");
+
+let Myfun = function(){
+    console.log("Asynchronous Code");
+}
+setTimeout(Myfun,2000);
+
+console.log("End");
+*/
+
+//Array fun in setTimeOut()
+//Ex:-3
+
+/*
+let Myfun = () => {console.log("hii samruddhi tai")};setTimeout(Myfun,5000)
+
+*/
+
+/*
+console.log("Start");
+let Display = function(){
+    console.log("My code Display");
+}
+
+let result = setTimeout(Display,2000);
+
+console.log("End");
+clearTimeout(result);
+*/
+
+// Array function
+
+let Display = () => {console.log("hii samruddhi tai")};
+let vari = setTimeout(Display,2000);
+clearTimeout(vari);
